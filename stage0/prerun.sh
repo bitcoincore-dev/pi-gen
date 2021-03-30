7a27ba5d54ad43bedff91c4c54b37c81d4a8756d
randymcmillan <randy.lee.mcmillan@gmail.com>
2021-03-30 18:15:43 -0400

    docker: build stage0 rmdir issue fix

--- stage0/prerun.sh
+++ stage0/prerun.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 
+#rm -rf   /pi-gen/work/2021-03-30-raspi/stage0/rootfs/debootstrap/*
+#touch    /pi-gen/work/2021-03-30-raspi/stage0/rootfs/debootstrap
+#rmdir -p /pi-gen/work/2021-03-30-raspi/stage0/rootfs/debootstrap
+#
+#rm -rf   /pi-gen/work/2021-03-30-raspi/stage0/rootfs/*
+#touch    /pi-gen/work/2021-03-30-raspi/stage0/rootfs
+#rmdir -p /pi-gen/work/2021-03-30-raspi/stage0/rootfs
+
 if [ ! -d "${ROOTFS_DIR}" ] || [ "${USE_QCOW2}" = "1" ]; then
 	bootstrap ${RELEASE} "${ROOTFS_DIR}" http://raspbian.raspberrypi.org/raspbian/
 fi
